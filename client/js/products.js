118222b57138d888137beea8e788d82f03d4a799
Ana Ascua <anaascua5@yahoo.com.ar>
2023-10-17 12:54:14 -0300

    creo products.js

---
+++ client/js/products.js
@@ -0,0 +1,58 @@
+const productos = [
+    {
+        id: 1,
+        productName: "taza arbustos",
+        price: 480,
+        quantity: 1,
+        img: "/client/media/Simpsons/arbustos.webp",
+    },
+    {
+        id: 2,
+        productName: "taza soy un burrito",
+        price: 1200,
+        quantity: 1,
+        img: "/client/media/Simpsons/burrito.webp",
+    },
+    {
+        id: 3,
+        productName: "taza chips",
+        price: 1700,
+        quantity: 1,
+        img: "/client/media/Simpsons/Chips.webp",
+    },
+    {
+        id: 4,
+        productName: "taza milhouseeee!",
+        price: 384,
+        quantity: 2,
+        img: "/client/media/Simpsons/milhouse.webp",
+    },
+    {
+        id: 5,
+        productName: "taza mister chispa",
+        price: 380,
+        quantity: 1,
+        img: "/client/media/Simpsons/misterChispa.webp",
+    },
+    {
+        id: 6,
+        productName: "taza homero",
+        price: 690,
+        quantity: 10,
+        img: "/client/media/Simpsons/Homero.webp",
+    },
+    {
+        id: 7,
+        productName: "taza mono",
+        price: 403,
+        quantity: 1,
+        img: "/client/media/Simpsons/monoHomero.webp",
+    },
+    {
+        id: 8,
+        productName: "taza lentes",
+        price: 1500,
+        quantity: 1,
+        img: "/client/media/Simpsons/noTeVoyAMentirMarge.webp"
+    }
+]
